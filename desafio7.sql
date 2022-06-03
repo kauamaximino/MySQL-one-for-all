288df4a0b6083b2c7a72110b67b30b3be4561e60
Kauã Maximino <kaua_maximino@outlook.com>
2022-06-03 15:02:54 -0300

    requisito 7 ok

---
+++ desafio7.sql
@@ -0,0 +1,15 @@
+SELECT
+    artists.name_artist AS artista,
+    album.name_album AS album,
+    COUNT(user_favorite_artist.user_id) AS seguidores
+FROM
+    artists
+    INNER JOIN album ON artists.id = album.artist_id
+    INNER JOIN user_favorite_artist ON artists.id = user_favorite_artist.artist_id
+GROUP BY
+    artists.name_artist,
+    album.name_album
+ORDER BY
+    seguidores DESC,
+    artista,
+    album;
