c27705b7d341691320feeeedc14d6d8a70492001
Kauã Maximino <kaua_maximino@outlook.com>
2022-06-03 15:28:16 -0300

    requisito 9 ok

---
+++ desafio9.sql
@@ -0,0 +1,9 @@
+SELECT
+    COUNT(*) AS quantidade_musicas_no_historico
+FROM
+    `user`
+    JOIN user_history ON user_history.user_id = user.id
+WHERE
+    `user`.`name` = "Bill"
+GROUP BY
+    `user`.`name`;
