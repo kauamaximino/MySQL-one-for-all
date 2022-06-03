09836022683b6cfa7ac063e15e68a115789b8973
Kauã Maximino <kaua_maximino@outlook.com>
2022-06-03 12:02:46 -0300

    requisito 5 ok

---
+++ desafio5.sql
@@ -0,0 +1,6 @@
+SELECT
+    song.`name_song` AS cancao,
+    COUNT(user_history.song_id) AS reproducoes
+FROM
+    song
+    JOIN user_history ON song.id = user_history.song_id GROUP BY cancao ORDER BY reproducoes DESC, cancao LIMIT 2;
